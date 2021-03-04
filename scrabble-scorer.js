b51ab794ad9af1bddf87d10d3e40119143861818
kipp3073 <kipp3073@gmail.com>
2021-03-04 03:26:55 +0000

    Edited the code to avoid double calling

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -8,16 +8,18 @@ const oldPointStructure= {
 8: ['J', 'X'],
 10: ['Q', 'Z']
 };
+
 let newPointStructure = {};
 function transform(obj) {
 for (let x in obj) {
 for (let i = 0; i < obj[x].length; i++) {
-newPointStructure[(obj[x][i]).toLowerCase()] = x;
-
+newPointStructure [(obj[x][i]).toLowerCase()] = parseInt (x);
 }
-newPointStructure[' '] = 0;
+newPointStructure[' '] = 0; 
 }}
 transform(oldPointStructure);
+///
+
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
@@ -40,14 +42,15 @@ function oldScrabbleScorer(word) {
 
 function initialPrompt() {
 const input = require ("readline-sync");
-let scoringAlgorithm = input.question(`Welcome to the Scrabble score calculator. Enter 'Stop' to quit.
+let question = input.question(`Welcome to the Scrabble score calculator. 
 Which scoring algorithm would you like to use?
 0 - Scrabble: The traditional scoring algorithm.
 1 - Simple Score: Each letter is worth 1 point.
 2 - Bonus Vowels: Vowels are 3 pts, consonants are 1pt
 Enter 0,1,2:`);
-return scoringAlgorithm;
+return question;
 }
+
 let simpleScore;
 
 let vowelBonusScore;
@@ -60,20 +63,20 @@ let scoringAlgorithms =
 name:"Scrabblescore",
 description: "The traditional scoring algorithm",
 scoreFunction:
-function (word) {
+function scrabbleScore(word) {
 let total=0;
 for (let i = 0; i < word.length; i++){
 
-total += Number(newPointStructure[word[i]]);
-}
+total += Number (newPointStructure[word[i]]);
+};
 return total;
 }
-},
+}, 
 {
 name: "simpleScore",
 description: "Each letter is worth 1 point",
 scoreFunction:
-function (word){
+function simpleScore (word){
 let total=0;
 for (let i = 0; i < word.length; i++) {
 total = word[i]===' '? total : total+1;
@@ -84,7 +87,7 @@ return total;
 name: "vowelBonusScore",
 description: "Vowels are 3 pts. consonants are 1 pts.",
 scoreFunction:
-function(word){
+function vowelBonusScore(word){
 let total=0;
 let vowels=['a','e','i','o','u'];
 for (let i = 0; i < word.length; i++) {
@@ -94,8 +97,10 @@ else if(word[i]===' '){
 }
 else {total += 1}}
 return total;
-}}
+}
+}
 ];
+
 function scorerPrompt() {
 const input = require('readline-sync');
 let word = input.question("Enter a word to play:");
@@ -111,12 +116,12 @@ if (score === '2' || score === '1'){
 console.log(`Using Algorithm : ${scoringAlgorithms[score].name}`);
 console.log(`Score for the word ${newWord} is: ${scoringAlgorithms[score].scoreFunction(newWord)}`);
 
-} else {
+}  else {
 
 console.log(`Using Algorithm : ${scoringAlgorithms[0].name}`);
-console.log(`Score for the word ${newWord} is: ${scoringAlgorithms[0].scoreFunction(newWord)}`)}}
+console.log(`Score for the word ${newWord} is: ${scoringAlgorithms[0].scoreFunction(newWord)}`)}
+}
 
-runProgram()
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
@@ -133,3 +138,4 @@ module.exports = {
 	runProgram: runProgram,
 	scorerPrompt: scorerPrompt
 };
+ 
